996a2e380dc5131428cde6b23b8021f40c7cc7ed
GreedoRen <greedo.it@gmail.com>
2020-07-19 22:16:05 +0300

    add joi, create validation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const Joi = require('joi')
+
 const app = express()
 const courses = [
     { id: 1, name: 'course_1' },
@@ -7,6 +9,8 @@ const courses = [
     { id: 4, name: 'course_4' },
     { id: 5, name: 'course_5' }
 ]
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hellow world')
@@ -20,6 +24,28 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(index => index.id === parseInt(req.params.id))
     if (!course) res.status(404).send('Invalid course ID')
     res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema)
+    console.log(result)
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course) // по стандарту переходим на запушенный курс
 })
 
 app.listen(3000, () => {
