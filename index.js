00104cebb546a81080386578c82c9d44291f561c
GreedoRen <greedo.it@gmail.com>
2020-07-28 07:50:19 +0300

    del

--- index.js
+++ index.js
@@ -22,17 +22,14 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(index => index.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('Invalid course ID')
+    if (!course) return res.status(404).send('Invalid course ID')
     res.send(course)
 })
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body)
 
-    if (error) {
-        res.status(400).send(error.details[0].message)
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message)
 
     const course = {
         id: courses.length + 1,
@@ -44,18 +41,14 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(index => index.id === parseInt(req.params.id))
-
-    if (!course) res.status(404).send('Invalid course ID')
+    if (!course) return res.status(404).send('Invalid course ID')
 
     const { error } = validateCourse(req.body)
-    if (error) {
-        res.status(400).send(error.details[0].message)
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message)
+
     course.name = req.body.name
 
     res.send(course)
-
 })
 
 function validateCourse(course) {
@@ -67,6 +60,17 @@ function validateCourse(course) {
 
     return val
 }
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(index => index.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('Invalid course ID')
+
+    const index = courses.indexOf(course)
+    courses.splice(index)
+
+    res.send(course)
+
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
