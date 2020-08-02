855026de025da9ddfcbd04850b8dfd026e3c1dea
GreedoRen <greedo.it@gmail.com>
2020-08-02 22:37:45 +0300

    i mangoose

---
+++ mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(e => console.log('MongoErr: ', e))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
