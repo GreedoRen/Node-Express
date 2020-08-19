be0857eda2bb301913cba60d329c1a302fd1589a
GreedoRen <greedo.it@gmail.com>
2020-08-19 08:20:51 +0300

    ref objectId

--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
+const rentals = require('./routes/rentals')
 const express = require('express')
 const app = express()
 
@@ -12,6 +14,7 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json())
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
+app.use('api/rentals', rentals)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
