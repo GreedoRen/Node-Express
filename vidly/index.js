738e7469f36124c5e8ffbe6cb96936bf1476a092
GreedoRen <greedo.it@gmail.com>
2020-08-19 08:43:27 +0300

    users route

--- vidly/index.js
+++ vidly/index.js
@@ -4,6 +4,8 @@ Joi.objectId = require('joi-objectid')(Joi)
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const rentals = require('./routes/rentals')
+const users = require('./routes/users')
+const movies = require('./routes/movies')
 const express = require('express')
 const app = express()
 
@@ -15,6 +17,8 @@ app.use(express.json())
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('api/rentals', rentals)
+app.use('api/movies', movies)
+app.use('api/users', users)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ vidly/routes/users.js
@@ -0,0 +1,10 @@
+const { User, validateUser } = require('../models/user')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+    const user = await User.find().sort('name')
+    res.send(user)
+})
+
+module.exports = router
