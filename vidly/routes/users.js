ffc500959b25d35513f5f227d4010cfe58307637
GreedoRen <greedo.it@gmail.com>
2020-08-20 08:18:35 +0300

    users

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -3,7 +3,19 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    const user = await User.find().sort('name')
+    const { error } = validateGenre(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    let user = await User.findOne({ email: req.body.email })
+    if (user) return res.status(400).send('User alredy registered')
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    await user.save()
+
     res.send(user)
 })
 
