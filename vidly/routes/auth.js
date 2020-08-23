faf9b36f4a08a6934ba080d695fb47dff403f098
GreedoRen <greedo.it@gmail.com>
2020-08-23 22:54:22 +0300

    auth

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,23 +1,30 @@
-const { User, validateUser } = require('../models/user')
+const { User } = require('../models/user')
 const _ = require('lodash')
 const bcrypt = require('bcrypt')
 const express = require('express')
 const mongoose = require('mongoose')
+const Joi = require('joi')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    const { error } = validateUser(req.body)
+    const { error } = validateAuth(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
     let user = await User.findOne({ email: req.body.email })
-    if (user) return res.status(400).send('User alredy registered')
+    if (!user) return res.status(400).send('Invalid email or password')
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']))
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(user.password, salt)
-    await user.save()
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(400).send('Invalid email or password')
 
-    res.send(_.pick(user, ['_id', 'name', 'email']))
+    res.send(true)
 })
+
+function validateAuth(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
+    return Joi.validate(req, schema)
+}
 
 module.exports = router
